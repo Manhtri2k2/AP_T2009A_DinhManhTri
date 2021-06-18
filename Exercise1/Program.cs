9ff5eb871117821f0954f35475ff5f925f9bf600
DinhManhTri <Tridmth2008060@fpt.edu.vn>
2021-06-18 15:19:50 +0700

    code

---
+++ Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 40;
+            person.Name = "Amar";
+            Console.WriteLine("Simple Properties Demo");
+            Console.WriteLine($"Person details - Name = {person.Name}, Age = {person.Age}");
+            Console.WriteLine($"Person details (After incrementing age) - Name = Amar, Age = {person.Age + 1}");
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string strWord;
+            string word;
+            int strt = 0;
+            int cnt = -1;
+            int idx = -1;
+
+            Console.WriteLine("Enter a string");
+            strWord = Console.ReadLine();
+            Console.WriteLine("Enter a word to search: ");
+            word = Console.ReadLine();
+
+
+            while (strt != -1)
+            {
+                strt = strWord.IndexOf(word, idx + 1);
+                cnt += 1;
+                idx = strt;
+            }
+
+            Console.Write($"Word found {cnt} times in the string", word);
+
+            Console.ReadKey();
+        }
+    }
+}
